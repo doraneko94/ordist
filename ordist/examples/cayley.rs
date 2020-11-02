2e63a7d4d6294379b531d8ca0427bacb51d7b86b
doraneko94 <shuntaro94@gmail.com>
2020-11-02 16:43:42 +0900

    fix example: kendall -> cayley

--- ordist/examples/cayley.rs
+++ ordist/examples/cayley.rs
@@ -3,5 +3,5 @@ use ordist::permutation::*;
 fn main() {
     let v1 = vec![1, 2, 3, 4, 5];
     let v2 = vec![3, 4, 1, 2, 5];
-    println!("{} == {}", kendall_dist(&v1, &v2).unwrap(), n_circle(&v1, &v2).unwrap());
+    println!("{} == {}", cayley_dist(&v1, &v2).unwrap(), v1.len() - n_circle(&v1, &v2).unwrap());
 }
